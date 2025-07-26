5c3cf9ef2411e831d72092a8b129f0d3a0306fb1
Tobias Nerger <tobias.nerger@gmail.com>
2025-07-26 10:32:35 +0200

    feat: Enhance Pagination component with disabled state for inactive links and add German translation for pagination

--- laravel/resources/js/Components/UI/Pagination.vue
+++ laravel/resources/js/Components/UI/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="gap-1 felx">
         <Link v-for="(link, index) in links" :key="index" class="px-4 py-2 rounded-md" :href="link.url ?? '#'"
-            :class="{ 'bg-red-700 text-gray-300': link.active }" v-html="link.label">
+            :class="{ 'bg-red-700 text-gray-300': link.active, 'pointer-events-none opacity-45': !link.url }" v-html="link.label">
         </Link>
     </div>
 </template>
---
+++ laravel/resources/lang/de/pagination.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'previous' => '&lt;',
+    'next' => '&gt;',
+];
