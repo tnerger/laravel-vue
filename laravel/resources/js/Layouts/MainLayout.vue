0f05749511184bee22811dc47642bd921b7ca665
Tobias Nerger <tobias.nerger@gmail.com>
2025-08-04 11:27:22 +0200

    feat: Update SVG icons for user actions and enhance layout for listings page

--- laravel/resources/js/Layouts/MainLayout.vue
+++ laravel/resources/js/Layouts/MainLayout.vue
@@ -1,32 +1,32 @@
 <template>
     <header class="bg-white dark:bg-gray-800 border-gray-200 border-b dark:border-b-gray-700 w-full">
         <div class="mx-auto container">
-            <nav class="flex justify-between items-center p-4">
-                <div class="font-medium text-lg">
-                    <Link :href="route('listing.index')">Listings</Link>
-                </div>
+            <nav class="flex justify-between items-center p-4 text-gray-500">
                 <div class="relative font-bold text-red-700 text-xl text-center">
                     <Link :href="route('listing.index')">HotPlaces</Link>
                     <figure class="top-0 -right-5.5 absolute opacity-45 rotate-25">🔥</figure>
                 </div>
                 <div class="flex items-center gap-4" v-if="user">
-                    <Link :href="route('notification.index')" class="relative py-2 pr-2 text-gray-500">
-                        <Bell class="fill-gray-500 w-5 h-6" />
-                        <div v-if="notificationCount" class="top-0 right-0 absolute bg-red-700 dark:bg-red-400 border border-white dark:border-gray-900 rounded-full w-5 h-5 font-medium text-white text-xs text-center">
-                            {{ notificationCount }}
-                        </div>
+                    <Link :href="route('notification.index')" class="relative py-2 pr-2">
+                    <Bell/>
+                    <div v-if="notificationCount"
+                        class="top-0 right-0 absolute bg-red-700 dark:bg-red-400 border border-white dark:border-gray-900 rounded-full w-5 h-5 font-medium text-white text-xs text-center">
+                        {{ notificationCount }}
+                    </div>
                     </Link>
 
-                    <Link :href="route('realtor.listing.index')" class="text-gray-500 text-sm">{{ user.name }} </Link>
-                    <Link :href="route('realtor.listing.create')" class="btn-primary">+ New Listing
+                    <Link :href="route('realtor.listing.index')" class="text-sm" alt="Go to user profile Settings">
+                    <UserProfile />
                     </Link>
                     <div>
-                        <Link :href="route('logout')" method="delete">Logout</Link>
+                        <Link :href="route('logout')" method="delete" class="cursor-pointer" alt="Sign Out"><SignOut /></Link>
                     </div>
                 </div>
                 <div v-else class="flex gap-2">
-                    <Link :href="route('user-account.create')">Register</Link>
-                    <Link :href="route('login')">Sign-In</Link>
+
+                    <Link :href="route('login')" alt="Sign in">
+                        <SignIn />
+                    </Link>
                 </div>
             </nav>
         </div>
@@ -43,6 +43,9 @@
 
 <script setup>
 import Bell from '@/Components/Images/Bell.vue';
+import UserProfile from '@/Components/Images/UserProfile.vue';
+import SignOut from '@/Components/Images/SignOut.vue';
+import SignIn from '@/Components/Images/SignIn.vue';
 import { Link, usePage } from '@inertiajs/vue3';
 import { computed } from 'vue';
 const props = defineProps({ message: String })
--- laravel/resources/js/Pages/Realtor/Index.vue
+++ laravel/resources/js/Pages/Realtor/Index.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1 class="mb-4 text-3xl">Your Listings</h1>
+
+    <div class="flex justify-between items-start">
+        <h1 class="mb-4 text-3xl"> Your Listings</h1>
+        <Link :href="route('realtor.listing.create')" class="btn-primary" alt="Add a new listing">+ New Listing
+        </Link>
+    </div>
     <section v-if="listings.data.length">
         <RealtorFilters :filter="filter" :sort="sort"></RealtorFilters>
     </section>
@@ -9,7 +14,9 @@
             :class="{ 'border-dashed dark:border-red-900 border-red-900': listing.deleted_at }">
             <div class="flex md:flex-row flex-col justify-between md:items-center gap-2">
                 <div :class="{ 'opacity-25': listing.deleted_at }">
-                    <div v-if="listing.sold_at !== null" class="inline-block mb-2 p-1 border border-green-300 dark:border-green-900 border-dashed rounded-md font-bold text-green-500 dark:text-green-900 text-xs uppercase">Sold</div>
+                    <div v-if="listing.sold_at !== null"
+                        class="inline-block mb-2 p-1 border border-green-300 dark:border-green-900 border-dashed rounded-md font-bold text-green-500 dark:text-green-900 text-xs uppercase">
+                        Sold</div>
                     <div class="xl:flex items-center gap-2">
                         <Price :price="listing.price" class="font-medium text-2xl"></Price>
                         <ListingSpace :listing="listing"></ListingSpace>
@@ -32,13 +39,15 @@
                             :href="route('realtor.listing.restore', listing)" method="PUT">Restore</Link>
                     </div>
                     <div class="mt-2">
-                        <Link class="block btn-outline w-full font-medium text-xs text-center" :href="route('realtor.listing.image.create', { listing })">
-                            Edit Images <span v-if="listing.images_count">({{ listing.images_count }})</span>
+                        <Link class="block btn-outline w-full font-medium text-xs text-center"
+                            :href="route('realtor.listing.image.create', { listing })">
+                        Edit Images <span v-if="listing.images_count">({{ listing.images_count }})</span>
                         </Link>
                     </div>
                     <div class="mt-2">
-                        <Link class="block btn-outline w-full font-medium text-xs text-center" :href="route('realtor.listing.show', { listing })">
-                            See offers <span v-if="listing.offers_count">({{ listing.offers_count }})</span>
+                        <Link class="block btn-outline w-full font-medium text-xs text-center"
+                            :href="route('realtor.listing.show', { listing })">
+                        See offers <span v-if="listing.offers_count">({{ listing.offers_count }})</span>
                         </Link>
                     </div>
                 </section>
